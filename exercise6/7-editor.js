0b7737a6885366b116374dbfd9bf8950751139a0
Mohammad Saleh Abbasi <abbasisaleh895@gmail.com>
2024-12-24 23:49:58 -0800

    change

--- exercise6/7-editor.js
+++ exercise6/7-editor.js
@@ -6,23 +6,7 @@ Look at editor.png, you should create a text editor similar to that.
 - The third button bolds the text when clicked.
 - The fourth button italicizes the text when clicked.
 */
-// const textarea = document.querySelector('#textarea');
-// const removeLastButton = document.querySelector('.removeLast');
-// const removeAll = document.querySelector('.removeAll');
-// const bold = document.querySelector('.bold');
-// const italic = document.querySelector('.italic');
 
-// removeLastButton.addEventListener("click", () => {
-//     const text = textarea.value;
-//     textarea.value = text.slice(0, -1); 
-// });
-
-
-// removeAll.addEventListener("click", () => {
-//     const text = textarea.value;
-//     textarea.value = " "
-// });
-//todo-1
 const removeLastButton = document.querySelector('.removeLast');
 removeLastButton.addEventListener('click', () => { 
     let editableDiv = document.querySelector('#textarea'); 
@@ -30,24 +14,24 @@ removeLastButton.addEventListener('click', () => {
     editableDiv.innerText = content.slice(0, -1); 
 }); 
 
-// //todo-2
+
 const removeAll = document.querySelector('.removeAll');
 removeAll.addEventListener('click', () => { 
     let editableDiv = document.querySelector('#textarea'); 
     editableDiv.innerText = ''; 
 });
 
-// //todo-3
-const boldTextBtn = document.querySelector('.bold');
-boldTextBtn.addEventListener('click', () => {
+
+const bold = document.querySelector('.bold');
+bold.addEventListener('click', () => {
     let editableDiv = document.querySelector('#textarea');
     let inputText = editableDiv.innerHTML;
     editableDiv.innerHTML = inputText.split(' ').map(word => `<b>${word}</b>`).join(' ');
 });
 
-// // //todo-4
-const italicTextBtn = document.querySelector('.italic');
-italicTextBtn.addEventListener('click', () => {
+
+const italic = document.querySelector('.italic');
+italic.addEventListener('click', () => {
     let editableDiv = document.querySelector('#textarea');
     let inputText = editableDiv.innerHTML;
     editableDiv.innerHTML = inputText.split(' ').map(word => `<em>${word}</em>`).join(' ');
