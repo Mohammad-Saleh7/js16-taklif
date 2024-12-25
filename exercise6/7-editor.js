94b0feec84d80e98fd2e24e91f4f520651ccc787
Mohammad Saleh Abbasi <abbasisaleh895@gmail.com>
2024-12-24 23:47:04 -0800

    changed

--- exercise6/7-editor.js
+++ exercise6/7-editor.js
@@ -6,24 +6,52 @@ Look at editor.png, you should create a text editor similar to that.
 - The third button bolds the text when clicked.
 - The fourth button italicizes the text when clicked.
 */
-const textarea = document.querySelector('.text');
+// const textarea = document.querySelector('#textarea');
+// const removeLastButton = document.querySelector('.removeLast');
+// const removeAll = document.querySelector('.removeAll');
+// const bold = document.querySelector('.bold');
+// const italic = document.querySelector('.italic');
+
+// removeLastButton.addEventListener("click", () => {
+//     const text = textarea.value;
+//     textarea.value = text.slice(0, -1); 
+// });
+
+
+// removeAll.addEventListener("click", () => {
+//     const text = textarea.value;
+//     textarea.value = " "
+// });
+//todo-1
 const removeLastButton = document.querySelector('.removeLast');
+removeLastButton.addEventListener('click', () => { 
+    let editableDiv = document.querySelector('#textarea'); 
+    let content = editableDiv.innerText;
+    editableDiv.innerText = content.slice(0, -1); 
+}); 
+
+// //todo-2
 const removeAll = document.querySelector('.removeAll');
-const bold = document.querySelector('.bold');
-const italic = document.querySelector('.italic');
-
-removeLastButton.addEventListener("click", () => {
-    const text = textarea.value;
-    textarea.value = text.slice(0, -1); 
+removeAll.addEventListener('click', () => { 
+    let editableDiv = document.querySelector('#textarea'); 
+    editableDiv.innerText = ''; 
 });
 
-
-removeAll.addEventListener("click", () => {
-    const text = textarea.value;
-    textarea.value = " "
+// //todo-3
+const boldTextBtn = document.querySelector('.bold');
+boldTextBtn.addEventListener('click', () => {
+    let editableDiv = document.querySelector('#textarea');
+    let inputText = editableDiv.innerHTML;
+    editableDiv.innerHTML = inputText.split(' ').map(word => `<b>${word}</b>`).join(' ');
 });
 
-
+// // //todo-4
+const italicTextBtn = document.querySelector('.italic');
+italicTextBtn.addEventListener('click', () => {
+    let editableDiv = document.querySelector('#textarea');
+    let inputText = editableDiv.innerHTML;
+    editableDiv.innerHTML = inputText.split(' ').map(word => `<em>${word}</em>`).join(' ');
+});
 
 
 
